2c589320bc57438401e3b3475c331f71213f29c7
corno <corno.astn.a@schraverus.com>
2022-09-06 13:45:57 +0200

    .

--- pub/src/index.ts
+++ pub/src/index.ts
@@ -2,6 +2,7 @@
 
 export * from "./functions/createArrayBuilder"
 export * from "./functions/createDictionaryBuilder"
+export * from "./functions/createUnsafeDictionaryBuilder"
 export * from "./functions/createStack"
 
 export * from "./types/ArrayBuilder"
